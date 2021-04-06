c437b78c317c853f1b4bd42324ba47b070a28f38
unknown <muzakisyahrul100@gmail.com>
2021-04-06 23:26:15 +0700

    first commit

---
+++ latihan1.php
@@ -0,0 +1,15 @@
+<?php
+// penulisan array
+$hari = array("Senin", "Selasa", "Rabu");
+$bulan = ["Januari", "Februari", "Maret"];
+$gabungan = [100, "teks", true];
+
+//memanggil array
+// versi debungging
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+
+//versi user
+echo $gabungan[0];
